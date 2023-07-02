1f3861378dd9f0e9982dbbbd691dcb2a8a4317c4
unknown <mdsazzadhosentamim95@gmail.com>
2023-07-02 11:08:03 +0600

    added layout

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,14 +1,33 @@
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import { getEventById } from "@/dummy-data";
 import { useRouter } from "next/router";
+import { Fragment } from "react";
 
 const EventDetailPage = () => {
 
     const router = useRouter();
 
-    console.log(router.pathname);
+    const eventId = router.query.eventId;
+    const event = getEventById(eventId);
+
+    if (!event) {
+        return <p>No event found!</p>
+    }
     // console.log(router.query);
     return (
         <div>
-            <h1>This is Event Detail page!</h1>
+            <Fragment />
+            <EventSummary title={event.title} />
+            <EventLogistics
+            date={event.date}
+            address={event.location}
+            image={event.image}
+            imageAlt={event.title}
+             />
+            <EventContent />
+            <p>{event.description}</p>
         </div>
     );
 };
